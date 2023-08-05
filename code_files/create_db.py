b4fc1f9c32e9f9a6f38a3d9baaa891713c253a62
Nishantj18 <113407865+Nishantj18@users.noreply.github.com>
2023-08-05 20:53:11 +0530

    Add files via upload

---
+++ code_files/create_db.py
@@ -0,0 +1,13 @@
+#Database File
+import sqlite3
+def create_db():
+    conn=sqlite3.connect(database="ResultManagementSystem.db")
+    cur=conn.cursor()
+#Table Creation for Student Page
+    cur.execute("Create table if not exists student1(roll INTEGER primary key AutoIncrement,name text,email test,gender text,dob text,contact text,admission text,course text,state text,city text,pin text,address text)")
+    conn.commit()
+
+
+    conn.close()
+
+create_db()
